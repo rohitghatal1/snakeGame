c72c03663a48efe961fa2db031110aece3bba09d
rohitghatal1 <rohitghatal@gmail.com>
2024-08-04 16:01:08 +0545

    Add project files.

---
+++ snakeGame/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace snakeGame.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
